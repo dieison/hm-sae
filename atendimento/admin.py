abaefacc02c7825c8b88f1ba55b8b6784a5770d9
Luan Prates <luanprates@edu.unifor.br>
2018-09-24 20:19:21 -0300

    alterando o model
    alterando o model

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ficha, Paciente
 
-# Register your models here.
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ['n_do_Prontuário', 'nome_do_Paciente']
+    search_fields = ['n_do_Prontuário', 'nome_do_Paciente']
+
+class FichaAdmin(admin.ModelAdmin):
+    list_display = ['paciente', 'data']
+    search_fields = ['paciente', 'data']
+
+admin.site.register(Ficha, FichaAdmin)
+admin.site.register(Paciente, PacienteAdmin)
--- atendimento/apps.py
+++ atendimento/apps.py
@@ -1,21 +1,5 @@
 from django.apps import AppConfig
-from django.contrib import admin
-from .models import Ficha, Paciente
 
 
 class AtendimentoConfig(AppConfig):
     name = 'atendimento'
-
-
-class PacienteAdmin(admin.ModelAdmin):
-    list_display = ['n_do_Prontuário', 'nome_do_Paciente']
-    search_fields = ['n_do_Prontuário', 'nome_do_Paciente']
-
-
-class FichaAdmin(admin.ModelAdmin):
-    list_display = ['paciente', 'data']
-    search_fields = ['paciente', 'data']
-
-
-admin.site.register(Ficha, FichaAdmin)
-admin.site.register(Paciente, PacienteAdmin)
