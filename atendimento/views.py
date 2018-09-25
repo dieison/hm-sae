4bdb54f99fb48093bd8080f7b9f5fee2e4bfa9ee
Luan Prates <luanprates@edu.unifor.br>
2018-09-25 19:49:17 -0300

    Criando lista de pacientes
    Criando lista de pacientes

--- atendimento/views.py
+++ atendimento/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Paciente
 # Create your views here.
+def lista_pacientes(request):
+    pacientes = Paciente.objects.all()
+    return render(request, 'pacientes.html', {'pacientes': pacientes})
--- hmsae/urls.py
+++ hmsae/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from atendimento import urls as atendimento_urls
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('paciente/', include(atendimento_urls)),
 ]
