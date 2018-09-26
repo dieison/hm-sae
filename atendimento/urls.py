15dd1ba4c70da3d54e780727228d567ebe72a964
Luan Prates <luanprates@edu.unifor.br>
2018-09-25 22:55:37 -0300

    Implementada alteração de paciente
    Implementada alteração de paciente

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import lista_pacientes
 from .views import novo_paciente
+from .views import alterar_paciente
 
 urlpatterns = [
     path('lista/', lista_pacientes, name='lista_pacientes'),
     path('novo/', novo_paciente, name='novo_paciente'),
+    path('alterar/<int:id>/', alterar_paciente, name='alterar_paciente'),
 ]
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Paciente
 from .forms import PacienteForm
 
@@ -9,9 +9,20 @@ def lista_pacientes(request):
 
 
 def novo_paciente(request):
-    form = PacienteForm(request.POST, None)
+    form = PacienteForm(request.POST or None)
 
     if form.is_valid():
         form.save()
         return redirect('lista_pacientes')
+    return render(request, 'formulario_paciente.html', {'form': form})
+
+
+def alterar_paciente(request, id):
+    pasiente = get_object_or_404(Paciente, pk=id)
+    form = PacienteForm(request.POST or None, instance=pasiente)
+
+    if form.is_valid():
+        form.save()
+        return redirect('lista_pacientes')
+
     return render(request, 'formulario_paciente.html', {'form': form})
