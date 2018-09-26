fedcba1c3e561995a8dce896be6e840798a91946
Luan Prates <luanprates@edu.unifor.br>
2018-09-25 23:20:42 -0300

    Implementada função excluir
    Implementada função excluir

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import lista_pacientes
 from .views import novo_paciente
 from .views import alterar_paciente
+from .views import excluir_paciente
 
 urlpatterns = [
     path('lista/', lista_pacientes, name='lista_pacientes'),
     path('novo/', novo_paciente, name='novo_paciente'),
     path('alterar/<int:id>/', alterar_paciente, name='alterar_paciente'),
+    path('excluir/<int:id>/', excluir_paciente, name='excluir_paciente'),
 ]
--- atendimento/views.py
+++ atendimento/views.py
@@ -18,11 +18,20 @@ def novo_paciente(request):
 
 
 def alterar_paciente(request, id):
-    pasiente = get_object_or_404(Paciente, pk=id)
-    form = PacienteForm(request.POST or None, instance=pasiente)
+    paciente = get_object_or_404(Paciente, pk=id)
+    form = PacienteForm(request.POST or None, instance=paciente)
 
     if form.is_valid():
         form.save()
         return redirect('lista_pacientes')
 
     return render(request, 'formulario_paciente.html', {'form': form})
+
+def excluir_paciente(request, id):
+    paciente = get_object_or_404(Paciente, pk=id)
+    form = PacienteForm(request.POST or None, instance=paciente)
+
+    if request.method == 'POST':
+        paciente.delete()
+        return redirect('lista_pacientes')
+    return render(request, 'confirmar_excluir_paciente.html', {'paciente': paciente})
