3a550217ad43cfd4d4fb1397875b23364ac6b13d
Luan Prates <luanprates@edu.unifor.br>
2018-09-25 23:26:21 -0300

    retirada linha não utilizada
    retirada linha não utilizada

--- atendimento/views.py
+++ atendimento/views.py
@@ -27,9 +27,9 @@ def alterar_paciente(request, id):
 
     return render(request, 'formulario_paciente.html', {'form': form})
 
+
 def excluir_paciente(request, id):
     paciente = get_object_or_404(Paciente, pk=id)
-    form = PacienteForm(request.POST or None, instance=paciente)
 
     if request.method == 'POST':
         paciente.delete()
