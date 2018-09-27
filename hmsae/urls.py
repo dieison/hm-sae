dfab4ee95d89b8f4e8c6f0155d8a283fc58742ee
Luan Prates <luanprates@edu.unifor.br>
2018-09-26 23:58:45 -0300

    implementando a pagina home
    implementando a pagina home

--- hmsae/urls.py
+++ hmsae/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from home import urls as home_urls
 from atendimento import urls as atendimento_urls
 
 
 
 urlpatterns = [
+    path('', include(home_urls)),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('login/', auth_views.LogoutView.as_view(), name='logout'),
     path('paciente/', include(atendimento_urls)),
 ]
